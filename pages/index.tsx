c7cfeca012c3b73684d888460c74b5f62b9abc98
fuu38 <contact@fumiya.biz>
2023-11-02 15:03:14 +0900

    add a Link to subpage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -7,6 +8,8 @@ export default function Home() {
   return (
       <main>
         <h2>SLannotate App</h2>
+        <Link href="/upload"><button>Upload</button></Link>
+        <Link href="/list"><button>List</button></Link>
       </main>
   )
 }
