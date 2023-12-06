3d62a6fb6d7780f05df250d394a4d0e0c890e979
fuu38 <contact@fumiya.biz>
2023-12-06 14:41:10 +0900

    use method chain

--- src/hooks/useVideoUploader.tsx
+++ src/hooks/useVideoUploader.tsx
@@ -4,40 +4,43 @@ import { API, Auth } from "aws-amplify";
 import { useRouter } from "next/router";
 
 export const useVideoUploader = () => {
-    const [video,setVideo] = useState<File|null>(null);
-    const router = useRouter();
-    const handleVideoChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-        if (!e.target.files) return;
-        const file = e.target.files[0];
-        console.log(file)
-        setVideo(file);
-    }
+  const [video, setVideo] = useState<File | null>(null);
+  const router = useRouter();
+  const handleVideoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    const file = e.target.files[0];
+    console.log(file);
+    setVideo(file);
+  };
 
-    const handleUpload = async () => {
-        const user = await Auth.currentAuthenticatedUser();
-        const userName = user.username;
-        const token = user.signInUserSession.idToken.jwtToken;
+  const handleUpload = async () => {
+    const user = await Auth.currentAuthenticatedUser();
+    const userName = user.username;
+    const token = user.signInUserSession.idToken.jwtToken;
 
-        const init = {
-          headers: {
-            Authorization: token,
-          },
-          body: video,
-        };
-        //upload file with using API
-        if (!video) return;
-        const result = await API.put(
-          "slannotate",
-          `users/${userName}/files/${video.name}/blob`,
-          init
-        );
-        //handle error
-        if (result.error) {
-            alert(`Failed to upload. Please try again ${result.error}`);
-        } else {
-          router.push(`/annotate/${video.name}`);//redirect to annotate page
-        }
-      };
+    
+    
+    const init = {
+      headers: {
+        Authorization: token,
+      },
+      body: video,
+    };
+    //upload file with using API
+    if (!video) return;
+    await API.put(
+      "slannotate",
+      `users/${userName}/files/${video.name}/blob`,
+      init
+    )
+      .then((res) => {
+        console.log(res);
+        router.push(`/annotate/${video.name}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-      return {video,handleVideoChange,handleUpload}
-}
+  return { video, handleVideoChange, handleUpload };
+};
